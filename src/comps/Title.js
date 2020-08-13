93fc257a5971163fd85ea5b7d8dc96942ec64cbd
NIKHIL SINGH <nikhil.4598@gmail.com>
2020-08-14 00:21:40 +0530

    starter-commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Walley</h1>
+      <h2>Your Pictures</h2>
+      <p>Upload n Enjoy.</p>
+    </div>
+  )
+}
+
+export default Title;
